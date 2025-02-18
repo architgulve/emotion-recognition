53df7b23be2246da9dc99674f57559b527c89911
AKSHAD KHUNE <akshadkhune@gmail.com>
2025-02-18 14:54:12 +0530

    quizPage basic

--- emotion-recognition/app/(tabs)/(training)/quizes.jsx
+++ emotion-recognition/app/(tabs)/(training)/quizes.jsx
@@ -1,34 +1,69 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import QuizComp from "../../../components/QuizQuestion";
 
 const Quizes = () => {
   return (
-    <SafeAreaView
-      style={{
-        backgroundColor: "#fffdee",
-      }}
-    >
+    <SafeAreaView style={{ backgroundColor: "#fffdee", flex: 1 }}>
       <View
-        style={{
-          backgroundColor: "#fffdee",
-          height: "100%",
-          alignItems: "center",
-        }}
+        style={{ backgroundColor: "#fffdee", flex: 1, alignItems: "center" }}
       >
+        {/* Header */}
         <View
           style={{
-            height: "100%",
+            height: "10%",
             width: "90%",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
-          <Text>
+          <Text style={{ fontSize: 30, color: "#162B47", fontWeight: "800" }}>
             QUIZES
           </Text>
         </View>
+
+ 
+        <View style={{ width: "90%", alignItems: "center"  }}>
+     
+          <QuizComp />
+
+          <View
+            style={{
+              flexDirection: "row",
+              width: "100%",
+              height: "30%",
+              justifyContent: "space-between",
+              marginTop: 10,
+            }}
+          >
+            <View
+              style={{
+                width: "48%",
+                height: "100%",
+                backgroundColor: "red",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text style={{ color: "white" }}>Image 1</Text>
+            </View>
+            <View
+              style={{
+                width: "48%",
+                height: "100%",
+                backgroundColor: "blue",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text style={{ color: "white" }}>Image 2</Text>
+            </View>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Quizes
+export default Quizes;
